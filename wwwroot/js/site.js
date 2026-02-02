2e73c2b627a4e553c3ba167cff17560f862c91f2
Angelica Carlos <angelicarlos@icloud.com>
2026-02-02 19:27:52 +1100

    add fyrefrontend

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.querySelectorAll("tr.table-row-link").forEach(row => {
+//         row.addEventListener("click", function (e) {
+//             // Ignore clicks on links or buttons
+//             if (e.target.closest("a, button")) return;
+
+//             window.location = row.dataset.href;
+//         });
+//     });
+// });
